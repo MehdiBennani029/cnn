82118cf72e757176df46aabeca128127407e1d4d
MehdiBennani029 <140610799+MehdiBennani029@users.noreply.github.com>
2025-12-12 16:12:28 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+// If your repo is: https://USERNAME.github.io/REPO_NAME/
+// then base MUST be "/REPO_NAME/"
+const repoName = "cnn";
+
 export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : `/${repoName}/`,
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
